4d8a2443dd5bcef3fb7492e7f8652dbd8294d17d
Francisco Gonzalez Ratti <franciscog.mk@gmail.com>
2019-12-23 16:13:13 -0300

    Added dynamic creation of resident array

--- schedule/src/views/ScheduleForm.vue
+++ schedule/src/views/ScheduleForm.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
+  <v-container id="step-1" class="py-0">
+    <v-row class="py-0">
+      <v-col class="py-0">
         <v-card dark max-width="400" flat color="#00796B" class="mx-auto py-1">
           <v-card-title class="headline">Numero de residentes</v-card-title>
           <v-slider
@@ -9,9 +9,22 @@
             :min="minWorkers"
             :max="maxWorkers"
             color="#80CBC4"
-            class="mx-6"
+            class="mx-6 my-0"
           ></v-slider>
-          <p class="mx-auto headline text-center">{{ workerNumber }}</p>
+          <p class="mx-auto mb-1 headline text-center">{{ workerNumber }}</p>
+        </v-card>
+        <v-card
+          v-for="worker in workerList"
+          v-bind:key="worker.id"
+          dark
+          max-width="400"
+          flat
+          color="#00796B"
+          class="mx-auto my-2 py-1"
+        >
+          <v-card-title class="title"
+            >Residente {{ worker.id + 1 }}</v-card-title
+          >
         </v-card>
       </v-col>
     </v-row>
@@ -24,8 +37,24 @@ export default {
     return {
       workerNumber: 0,
       minWorkers: 0,
-      maxWorkers: 10
+      maxWorkers: 10,
+      formStep: 1
     };
+  },
+  computed: {
+    workerList() {
+      let worker = {
+        name: "",
+        id: 0
+      };
+      let baseList = Array(this.workerNumber).fill(worker);
+      let populatedList = baseList.map((i, index) => {
+        let temp = Object.assign({}, i);
+        temp.id = index;
+        return temp;
+      });
+      return populatedList;
+    }
   }
 };
 </script>
