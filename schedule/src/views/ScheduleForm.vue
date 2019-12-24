ddb2ceda7b75353256386c4e41286cd21324ab58
Francisco Gonzalez <franciscog.mk@gmail.com>
2019-12-24 12:49:18 -0300

    Added animations

--- schedule/src/views/ScheduleForm.vue
+++ schedule/src/views/ScheduleForm.vue
@@ -6,8 +6,13 @@
       fluid
       class="py-0"
     >
-      <v-expand-x-transition>
-        <v-row v-if="formStep == worker.id" class="py-0">
+      <transition
+        v-if="formStep == worker.id"
+        appear
+        name="form-animation"
+        enter-active-class="animated fadeIn fast"
+      >
+        <v-row class="py-0">
           <v-col class="py-0">
             <h2
               class="mx-auto mt-0 mb-4 headline text-center white--text font-weight-bold"
@@ -32,102 +37,130 @@
             </v-card>
           </v-col>
         </v-row>
-      </v-expand-x-transition>
+      </transition>
     </v-container>
     <v-container fluid class="pt-0 mb-10">
-      <v-expand-x-transition>
-        <v-row v-if="formStep == 0" class="py-0">
-          <v-col class="py-0">
-            <h2
-              class="mx-auto mt-0 mb-4 headline text-center white--text font-weight-bold"
+      <v-row v-show="formStep == 0" class="py-0">
+        <v-col class="py-0">
+          <h2
+            class="mx-auto mt-0 mb-4 headline text-center white--text font-weight-bold"
+          >
+            PASO 1
+          </h2>
+          <v-card
+            max-width="600"
+            min-width="310"
+            flat
+            color="white"
+            class="mx-auto py-1"
+          >
+            <v-card-title style="color:#004D40" class="headline mx-auto"
+              >Numero de residentes</v-card-title
             >
-              PASO 1
-            </h2>
+            <v-slider
+              v-model="workerNumber"
+              :min="minWorkers"
+              :max="maxWorkers"
+              color="teal darken-3"
+              track-color="teal lighten-3"
+              class="mx-4 my-0"
+            >
+              <template v-slot:append>
+                <v-text-field
+                  v-model="workerNumber"
+                  :rules="workerNumberConstraint"
+                  class="mt-0 pt-0"
+                  hide-details
+                  single-line
+                  type="number"
+                  style="width: 35px"
+                ></v-text-field>
+              </template>
+            </v-slider>
+          </v-card>
+          <transition
+            appear
+            name="card-animation"
+            enter-active-class="animated fadeInDown faster"
+            leave-active-class="animated fadeOutUp faster"
+            v-for="worker in workerList"
+            v-bind:key="worker.id"
+          >
             <v-card
-              max-width="600"
-              min-width="310"
+              max-width="500"
               flat
               color="white"
-              class="mx-auto py-1"
+              class="mx-auto my-2 py-1"
             >
-              <v-card-title style="color:#004D40" class="headline mx-auto"
-                >Numero de residentes</v-card-title
+              <v-card-title style="color:#004D40" class="title"
+                >Residente {{ worker.id }}</v-card-title
               >
-              <v-slider
-                v-model="workerNumber"
-                :min="minWorkers"
-                :max="maxWorkers"
-                color="teal darken-3"
-                track-color="teal lighten-3"
-                class="mx-4 my-0"
-              >
-                <template v-slot:append>
-                  <v-text-field
-                    v-model="workerNumber"
-                    :rules="workerNumberConstraint"
-                    class="mt-0 pt-0"
-                    hide-details
-                    single-line
-                    type="number"
-                    style="width: 35px"
-                  ></v-text-field>
-                </template>
-              </v-slider>
+              <v-text-field
+                class="mx-4"
+                filled
+                label="Nombre"
+                v-model="worker.name"
+                :rules="nameConstraint"
+                counter="32"
+              ></v-text-field>
             </v-card>
-            <v-expand-transition
-              v-for="worker in workerList"
-              v-bind:key="worker.id"
-            >
-              <v-card
-                max-width="500"
-                flat
-                color="white"
-                class="mx-auto my-2 py-1"
-              >
-                <v-card-title style="color:#004D40" class="title"
-                  >Residente {{ worker.id }}</v-card-title
-                >
-                <v-text-field
-                  class="mx-4"
-                  filled
-                  label="Nombre"
-                  v-model="worker.name"
-                  :rules="nameConstraint"
-                  counter="32"
-                ></v-text-field>
-              </v-card>
-            </v-expand-transition>
-          </v-col>
-        </v-row>
-      </v-expand-x-transition>
-    </v-container>
-    <v-footer fixed class="mx-auto py-0 px-" color="teal darken-3">
-      <v-row align-content="center" class="mx-auto" id="nav-var">
-        <v-spacer></v-spacer>
-        <v-col v-if="formStep > 0" cols="auto" class="px-6">
-          <v-btn
-            fab
-            elevation="0"
-            color="white"
-            class="my-0"
-            @click="previousStep"
-          >
-            <v-icon dark large color="teal darken-3">mdi-chevron-left</v-icon>
-          </v-btn>
+          </transition>
         </v-col>
-        <v-col v-if="formStep > 0" cols="auto" class="px-0">
-          <v-btn fab elevation="0" color="white" class="my-0" @click="goHome">
-            <v-icon dark large color="teal darken-3">mdi-home</v-icon>
-          </v-btn>
-        </v-col>
-        <v-col v-if="formStep < workerNumber" cols="auto" class="px-6 py-2">
-          <v-btn fab elevation="0" color="white" class="my-0" @click="nextStep">
-            <v-icon dark large color="teal darken-3">mdi-chevron-right</v-icon>
-          </v-btn>
-        </v-col>
-        <v-spacer></v-spacer>
       </v-row>
-    </v-footer>
+    </v-container>
+    <transition
+      appear
+      name="footer-animation"
+      enter-active-class="animated fadeInUp faster"
+      leave-active-class="animated fadeOutDown faster"
+    >
+      <v-footer
+        v-if="workerNumber > 0"
+        fixed
+        class="mx-auto py-0"
+        color="teal darken-3"
+      >
+        <v-row align-content="center" class="mx-auto" id="nav-var">
+          <v-spacer></v-spacer>
+          <v-col v-show="formStep > 0" cols="auto" class="px-6">
+            <v-btn
+              fab
+              elevation="0"
+              color="white"
+              class="my-0 animated fadeIn fast"
+              @click="previousStep"
+            >
+              <v-icon dark large color="teal darken-3">mdi-chevron-left</v-icon>
+            </v-btn>
+          </v-col>
+          <v-col v-show="formStep > 0" cols="auto" class="px-0">
+            <v-btn
+              fab
+              elevation="0"
+              color="white"
+              class="my-0 animated fadeIn fast"
+              @click="goHome"
+            >
+              <v-icon dark large color="teal darken-3">mdi-home</v-icon>
+            </v-btn>
+          </v-col>
+          <v-col v-show="formStep < workerNumber" cols="auto" class="px-6">
+            <v-btn
+              fab
+              elevation="0"
+              color="white"
+              class="my-0 animated fadeIn fast"
+              @click="nextStep"
+            >
+              <v-icon dark large color="teal darken-3"
+                >mdi-chevron-right</v-icon
+              >
+            </v-btn>
+          </v-col>
+          <v-spacer></v-spacer>
+        </v-row>
+      </v-footer>
+    </transition>
   </v-form>
 </template>
 
